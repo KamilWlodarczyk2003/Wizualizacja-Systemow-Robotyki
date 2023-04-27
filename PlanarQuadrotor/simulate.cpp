afab00e3e6a45182d761d7b95c7f13623358889c
Marek Grzegorek <grzegorek.marek@zoho.com>
2023-04-27 19:28:49 +0200

    Fix indention in simulate.cpp

--- PlanarQuadrotor/simulate.cpp
+++ PlanarQuadrotor/simulate.cpp
@@ -5,7 +5,7 @@
 
 Eigen::MatrixXf LQR(PlanarQuadrotor &quadrotor, float dt) {
     /* Calculate LQR gain matrix */
-	Eigen::MatrixXf Eye = Eigen::MatrixXf::Identity(6, 6);
+    Eigen::MatrixXf Eye = Eigen::MatrixXf::Identity(6, 6);
     Eigen::MatrixXf A = Eigen::MatrixXf::Zero(6, 6);
     Eigen::MatrixXf A_discrete = Eigen::MatrixXf::Zero(6, 6);
     Eigen::MatrixXf B(6, 2);
@@ -33,99 +33,99 @@ void control(PlanarQuadrotor &quadrotor, const Eigen::MatrixXf &K) {
 
 int main(int argc, char* args[])
 {
-	std::shared_ptr<SDL_Window> gWindow = nullptr;
-	std::shared_ptr<SDL_Renderer> gRenderer = nullptr;
-	const int SCREEN_WIDTH = 1280;
-	const int SCREEN_HEIGHT = 720;
+    std::shared_ptr<SDL_Window> gWindow = nullptr;
+    std::shared_ptr<SDL_Renderer> gRenderer = nullptr;
+    const int SCREEN_WIDTH = 1280;
+    const int SCREEN_HEIGHT = 720;
 
-	/**
-	 * TODO: Extend simulation
-	 * 1. Set goal state of the mouse when clicking left mouse button (transform the coordinates to the quadrotor world! see visualizer TODO list)
-	 *    [x, y, 0, 0, 0, 0]
-	 * 2. Update PlanarQuadrotor from simulation when goal is changed
-	*/
-	Eigen::VectorXf initial_state = Eigen::VectorXf::Zero(6);
-	PlanarQuadrotor quadrotor(initial_state);
-	PlanarQuadrotorVisualizer quadrotor_visualizer(&quadrotor);
-	/**
-	 * Goal pose for the quadrotor
-	 * [x, y, theta, x_dot, y_dot, theta_dot]
-	 * For implemented LQR controller, it has to be [x, y, 0, 0, 0, 0]
-	*/
-	Eigen::VectorXf goal_state = Eigen::VectorXf::Zero(6);
-	goal_state << -1, 7, 0, 0, 0, 0;
-	quadrotor.SetGoal(goal_state);
-	/* Timestep for the simulation */
-	const float dt = 0.001;
-	Eigen::MatrixXf K = LQR(quadrotor, dt);
-	Eigen::Vector2f input = Eigen::Vector2f::Zero(2);
+    /**
+     * TODO: Extend simulation
+     * 1. Set goal state of the mouse when clicking left mouse button (transform the coordinates to the quadrotor world! see visualizer TODO list)
+     *    [x, y, 0, 0, 0, 0]
+     * 2. Update PlanarQuadrotor from simulation when goal is changed
+    */
+    Eigen::VectorXf initial_state = Eigen::VectorXf::Zero(6);
+    PlanarQuadrotor quadrotor(initial_state);
+    PlanarQuadrotorVisualizer quadrotor_visualizer(&quadrotor);
+    /**
+     * Goal pose for the quadrotor
+     * [x, y, theta, x_dot, y_dot, theta_dot]
+     * For implemented LQR controller, it has to be [x, y, 0, 0, 0, 0]
+    */
+    Eigen::VectorXf goal_state = Eigen::VectorXf::Zero(6);
+    goal_state << -1, 7, 0, 0, 0, 0;
+    quadrotor.SetGoal(goal_state);
+    /* Timestep for the simulation */
+    const float dt = 0.001;
+    Eigen::MatrixXf K = LQR(quadrotor, dt);
+    Eigen::Vector2f input = Eigen::Vector2f::Zero(2);
 
-	/**
-	 * TODO: Plot x, y, theta over time
-	 * 1. Update x, y, theta history vectors to store trajectory of the quadrotor
-	 * 2. Plot trajectory using matplot++ when key 'p' is clicked
-	*/
+    /**
+     * TODO: Plot x, y, theta over time
+     * 1. Update x, y, theta history vectors to store trajectory of the quadrotor
+     * 2. Plot trajectory using matplot++ when key 'p' is clicked
+    */
     std::vector<float> x_history;
     std::vector<float> y_history;
     std::vector<float> theta_history;
 
-	if (init(gWindow, gRenderer, SCREEN_WIDTH, SCREEN_HEIGHT) >= 0)
-	{
-		SDL_Event e;
-		bool quit = false;
-		float delay;
-		int x, y;
-		Eigen::VectorXf state = Eigen::VectorXf::Zero(6);
+    if (init(gWindow, gRenderer, SCREEN_WIDTH, SCREEN_HEIGHT) >= 0)
+    {
+        SDL_Event e;
+        bool quit = false;
+        float delay;
+        int x, y;
+        Eigen::VectorXf state = Eigen::VectorXf::Zero(6);
 
-		while (!quit)
-		{
-			//events
-			while (SDL_PollEvent(&e) != 0)
-			{
-				if (e.type == SDL_QUIT)
-				{
-					quit = true;
-				}
-				else if (e.type == SDL_MOUSEMOTION)
-				{
-					SDL_GetMouseState(&x, &y);
-					std::cout << "Mouse position: (" << x << ", " << y << ")" << std::endl;
-				}
+        while (!quit)
+        {
+            //events
+            while (SDL_PollEvent(&e) != 0)
+            {
+                if (e.type == SDL_QUIT)
+                {
+                    quit = true;
+                }
+                else if (e.type == SDL_MOUSEMOTION)
+                {
+                    SDL_GetMouseState(&x, &y);
+                    std::cout << "Mouse position: (" << x << ", " << y << ")" << std::endl;
+                }
                 
-			}
+            }
 
-			SDL_Delay((int) dt * 1000);
+            SDL_Delay((int) dt * 1000);
 
-			SDL_SetRenderDrawColor(gRenderer.get(), 0xFF, 0xFF, 0xFF, 0xFF);
-			SDL_RenderClear(gRenderer.get());
+            SDL_SetRenderDrawColor(gRenderer.get(), 0xFF, 0xFF, 0xFF, 0xFF);
+            SDL_RenderClear(gRenderer.get());
 
-			/* Quadrotor rendering step */
-			quadrotor_visualizer.render(gRenderer);
+            /* Quadrotor rendering step */
+            quadrotor_visualizer.render(gRenderer);
 
-			SDL_RenderPresent(gRenderer.get());
+            SDL_RenderPresent(gRenderer.get());
 
-			/* Simulate quadrotor forward in time */
-			control(quadrotor, K);
-			quadrotor.Update(dt);
-		}
-	}
-	SDL_Quit();
-	return 0;
+            /* Simulate quadrotor forward in time */
+            control(quadrotor, K);
+            quadrotor.Update(dt);
+        }
+    }
+    SDL_Quit();
+    return 0;
 }
 
 int init(std::shared_ptr<SDL_Window>& gWindow, std::shared_ptr<SDL_Renderer>& gRenderer, const int SCREEN_WIDTH, const int SCREEN_HEIGHT)
 {
-	if (SDL_Init(SDL_INIT_VIDEO) >= 0)
-	{
-		SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
-		gWindow = std::shared_ptr<SDL_Window>(SDL_CreateWindow("Planar Quadrotor", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN), SDL_DestroyWindow);
-		gRenderer = std::shared_ptr<SDL_Renderer>(SDL_CreateRenderer(gWindow.get(), -1, SDL_RENDERER_ACCELERATED), SDL_DestroyRenderer);
-		SDL_SetRenderDrawColor(gRenderer.get(), 0xFF, 0xFF, 0xFF, 0xFF);
-	}
-	else
-	{
-		std::cout << "SDL_ERROR: " << SDL_GetError() << std::endl;
-		return -1;
-	}
-	return 0;
+    if (SDL_Init(SDL_INIT_VIDEO) >= 0)
+    {
+        SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
+        gWindow = std::shared_ptr<SDL_Window>(SDL_CreateWindow("Planar Quadrotor", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN), SDL_DestroyWindow);
+        gRenderer = std::shared_ptr<SDL_Renderer>(SDL_CreateRenderer(gWindow.get(), -1, SDL_RENDERER_ACCELERATED), SDL_DestroyRenderer);
+        SDL_SetRenderDrawColor(gRenderer.get(), 0xFF, 0xFF, 0xFF, 0xFF);
+    }
+    else
+    {
+        std::cout << "SDL_ERROR: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+    return 0;
 }
